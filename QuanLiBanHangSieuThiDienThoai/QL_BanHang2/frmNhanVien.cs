cae23348c0b32f0b06d73c14b93a49b60a858ad5
datmanhtuan <71332778+datmanhtuan@users.noreply.github.com>
2020-09-11 19:56:00 +0700

    đạt

---
+++ QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmNhanVien.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+namespace QuanLyBanHang
+{
+    public partial class frmNhanVien : Form
+    {
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+        NhanVien nv = new NhanVien();
+        int chon = 0;
+        void KhoaDieuKhien()
+        {
+            txtTenDn.Enabled = txtMatKhau.Enabled = txtTenNV.Enabled = txtDiaChi.Enabled = txtSDT.Enabled = cbGT.Enabled = false;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = true;
+            btnLuu.Enabled = false;
+        }
+        void MoDieuKhien()
+        {
+            txtTenDn.Enabled = txtMatKhau.Enabled = txtTenNV.Enabled = txtDiaChi.Enabled = txtSDT.Enabled = cbGT.Enabled = true;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+        }
+        void SetNull()
+        {
+            txtTenDn.Text = txtMatKhau.Text = txtTenNV.Text = txtSDT.Text = txtDiaChi.Text = cbGT.Text = "";
+            tscbGT.Text = tstxtDiaChi.Text = tstxtMa.Text = tstxtTen.Text = "";
+        }
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = nv.HienThiNhanVien();
+            dgvNhanVien.DataSource = dt;
+            KhoaDieuKhien();
+        }
+
+    
