0a03d3131060bf3d593e03bc1a859ea3a5b1312d
datmanhtuan <71332778+datmanhtuan@users.noreply.github.com>
2020-09-12 22:56:59 +0700

    đạt

--- QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmNhanVien.cs
+++ QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmNhanVien.cs
@@ -45,3 +45,115 @@ namespace QuanLyBanHang
         }
 
     
+ private void btnThem_Click(object sender, EventArgs e)
+        {
+            MoDieuKhien();
+            SetNull();
+            chon = 1;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            MoDieuKhien();
+            chon = 2;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtTenDn.Text == "")
+                MessageBox.Show("Chọn nhân viên!");
+            else
+                if (DialogResult.Yes == MessageBox.Show("Bạn muốn xóa nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                {
+                    nv.XoaNhanVien(txtTenDn.Text);
+                    MessageBox.Show("Xóa thành công!");
+                    frmNhanVien_Load(sender, e);
+                    SetNull();
+                }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (chon == 1)
+            {
+                if (txtMatKhau.Text == "" || txtTenNV.Text == "" || txtTenDn.Text == "" || txtDiaChi.Text == "" || txtSDT.Text == "" || cbGT.Text == "")
+                    MessageBox.Show("Mời nhập đầy đủ thông tin!");
+                else
+                    if (DialogResult.Yes == MessageBox.Show("Bạn có muốn thêm nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        nv.ThemNhanVien(txtTenDn.Text,txtMatKhau.Text,txtTenNV.Text,cbGT.Text,txtDiaChi.Text,txtSDT.Text);
+                        MessageBox.Show("Thêm thành công!");
+                        SetNull();
+                        frmNhanVien_Load(sender, e);
+                    }
+            }
+            else if (chon == 2)
+            {
+                if (txtMatKhau.Text == "" || txtTenNV.Text == "" || txtTenDn.Text == "" || txtDiaChi.Text == "" || txtSDT.Text == "" || cbGT.Text == "")
+                    MessageBox.Show("Mời nhập đầy đủ thông tin!");
+                else
+                    if (DialogResult.Yes == MessageBox.Show("Bạn có muốn Sửa nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        nv.SuaNhanVien(txtTenDn.Text, txtMatKhau.Text, txtTenNV.Text, cbGT.Text, txtDiaChi.Text, txtSDT.Text);
+                        MessageBox.Show("Sửa thành công!");
+                        SetNull();
+                        frmNhanVien_Load(sender, e);
+                    }
+            }
+        }
+        private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                
+                txtTenDn.Text = dgvNhanVien.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtMatKhau.Text = dgvNhanVien.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtTenNV.Text = dgvNhanVien.Rows[e.RowIndex].Cells[2].Value.ToString();
+                cbGT.Text = dgvNhanVien.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtDiaChi.Text = dgvNhanVien.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtSDT.Text = dgvNhanVien.Rows[e.RowIndex].Cells[5].Value.ToString();
+            }
+            catch { }
+        }
+
+        private void tstxtMa_TextChanged(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = nv.TKTenDN(tstxtMa.Text);
+            //KhoiTao();
+        }
+        private void tstxtTen_TextChanged(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = nv.TKTenNV(tstxtTen.Text);
+            //KhoiTao();
+        }
+
+        private void tscbGT_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = nv.TKGTNhanVien(tscbGT.Text);
+            //KhoiTao();
+        }
+
+        private void tstxtDiaChi_TextChanged(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = nv.TKDiaChiNhanVien(tstxtDiaChi.Text);
+            //KhoiTao();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            frmNhanVien_Load(sender, e);
+            SetNull();
+            chon = 0;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
