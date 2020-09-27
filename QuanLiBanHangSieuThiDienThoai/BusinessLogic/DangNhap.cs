e7ae2a55610174c1ce5caec62adcfa93d761b812
DungLeMTA <duydung.glhd99@gmail.com>
2020-09-27 15:57:57 +0700

    first commit

---
+++ QuanLiBanHangSieuThiDienThoai/BusinessLogic/DangNhap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class DangNhap
+    {
+        public bool Login(string _Name, string _Pass)
+        {
+            string sql = @"select *  from NHANVIEN WHERE TenDN = '" + _Name + "' AND MatKhau =  CONVERT(NVARCHAR(32),HASHBYTES('MD5','" + _Pass + "'),2)";
+
+            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
+            SqlDataAdapter ad = new SqlDataAdapter(sql, con);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ QuanLiBanHangSieuThiDienThoai/BusinessLogic/KetNoiDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class KetNoiDB
+    {
+        SqlConnection cn = new SqlConnection();
+        static public String getconnect()
+        {
+            return (@"Data Source=DESKTOP-SAU51IQ;Initial Catalog=Bai6_BanHangSieuThi;Integrated Security=True");
+
+        }
+    }
+}
