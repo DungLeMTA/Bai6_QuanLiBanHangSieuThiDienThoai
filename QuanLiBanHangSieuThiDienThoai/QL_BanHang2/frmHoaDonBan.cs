d84570f168b5c2cf2ba288fd9b05add4ffbb4716
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-27 18:22:26 +0700

    Add files via upload

--- QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmHoaDonBan.cs
+++ QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmHoaDonBan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+  
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,7 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms; 
 
 namespace QuanLiBanHang
 {
