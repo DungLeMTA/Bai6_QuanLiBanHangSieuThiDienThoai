2942c1d85626ff5df16cc422b042b65572066729
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-28 07:21:58 +0700

    Update frmBanHang.cs

--- QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmBanHang.cs
+++ QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmBanHang.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLiBanHang
+namespace QuanLiBanHang   
 {
     public partial class frmBanHang : Form
     {
