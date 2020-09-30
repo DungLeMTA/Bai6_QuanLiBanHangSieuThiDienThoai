a96af9a3e69262094b0a83cd68174958da3e4027
trananh99 <54661281+trananh99@users.noreply.github.com>
2020-09-30 09:06:29 +0700

    Add files via upload

--- QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmBanHang.cs
+++ QuanLiBanHangSieuThiDienThoai/QL_BanHang2/frmBanHang.cs
@@ -16,5 +16,90 @@ namespace QuanLiBanHang
         {
             InitializeComponent();
         }
+
+        public void btnGiamSL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnTangSL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnLuu_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void HuyBo_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void btnOKKH_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnrefreshKH_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void dgvKhachHang_CellClick(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void txttenKH_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void txtTenNSX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void cmbTenLH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void txtTenSP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void dgvSanPham_CellClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnChonSP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void frmHoaDonBanHang_Load(object sender, EventArgs e)
+        {
+
+        }
+        
     }
+
 }
